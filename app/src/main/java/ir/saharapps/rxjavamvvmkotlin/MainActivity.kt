e15a451ee72cf8329e37459af1281689d475a7ef
sahar-as <sahar.asadian90@gmail.com>
2022-05-01 13:05:09 +0430

    getting data with Retrofit

--- app/src/main/java/ir/saharapps/rxjavamvvmkotlin/MainActivity.kt
+++ app/src/main/java/ir/saharapps/rxjavamvvmkotlin/MainActivity.kt
@@ -2,10 +2,21 @@ package ir.saharapps.rxjavamvvmkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.ViewModelProvider
+import ir.saharapps.rxjavamvvmkotlin.viewmodel.ApplicationViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    var applicationViewModel: ApplicationViewModel? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        applicationViewModel = ViewModelProvider(this)[ApplicationViewModel::class.java]
+        applicationViewModel!!.getPostList.observe(this) { postModles ->
+            Log.d("MainActivity", "onCreate: PostList: " + postModles[0].userId)
+        }
     }
 }
