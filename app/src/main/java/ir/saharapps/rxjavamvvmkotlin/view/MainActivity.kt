305aaa667a863bb2bf984ae05c035048ad1e8950
sahar-as <sahar.asadian90@gmail.com>
2022-05-02 15:18:57 +0430

    adding extentions

--- app/src/main/java/ir/saharapps/rxjavamvvmkotlin/view/MainActivity.kt
+++ app/src/main/java/ir/saharapps/rxjavamvvmkotlin/view/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.recyclerview.widget.RecyclerView
 import ir.saharapps.rxjavamvvmkotlin.R
 import ir.saharapps.rxjavamvvmkotlin.adapter.PostItemAdapter
 import ir.saharapps.rxjavamvvmkotlin.viewmodel.ApplicationViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     //design and implemented by Sahar Asadian
@@ -18,21 +19,19 @@ class MainActivity : AppCompatActivity() {
     var applicationViewModel: ApplicationViewModel? = null
 
     var adapter: PostItemAdapter? = null
-    var recyclerView: RecyclerView? = null
     var progressBar: ProgressBar? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         progressBar = findViewById(R.id.progressBar)
-        recyclerView = findViewById(R.id.rv_MainActivity_RecyclerView)
-        recyclerView!!.layoutManager = LinearLayoutManager(this)
+        rv_MainActivity_RecyclerView!!.layoutManager = LinearLayoutManager(this)
 
         applicationViewModel = ViewModelProvider(this)[ApplicationViewModel::class.java]
         applicationViewModel!!.getPostList.observe(this) { postModles ->
             adapter = PostItemAdapter(this, postModles)
             adapter!!.notifyDataSetChanged()
-            recyclerView!!.adapter = adapter
+            rv_MainActivity_RecyclerView!!.adapter = adapter
             progressBar!!.visibility = View.GONE
         }
     }
